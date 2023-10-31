e0d0d9870e0be787edc758e8049309acaf4f7213
Calm Dang <dangphuoccuongsn2k2@gmail.com>
2023-10-31 20:32:29 +0700

    EX1 LAB2 _add testbench

--- FSM_EX1/test/driver.cpp
+++ FSM_EX1/test/driver.cpp
@@ -1,7 +1,15 @@
-#define MAX_SIM 2000
+#define MAX_SIM 20
 
 void set_random(Vtop *dut, vluint64_t sim_unit) {
 	dut->w_i = rand() % 2;
 	dut->res_ni = (sim_unit > 3) ? 1 : 0    ;
+
+	static int stage = dut->Stage_o ;
+	static int count = 0 ; 
+	if( sim_unit >3 ){
+		printf("%d: %d -> %d \n",count,stage,dut->Stage_o) ;
+		stage = dut->Stage_o ;
+		count++ ;
+	}
 }
 
